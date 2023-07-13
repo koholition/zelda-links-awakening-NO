3d1109afe22a4112f8dbcaab5aedda108dc6a6cc
Tobias V. Langhoff <tobias@langhoff.no>
2023-07-13 17:22:33 +0200

    Oversatt filkopiering og -sletting

--- revisions/N0/src/data/file_menu_quit_ok.asm
+++ revisions/N0/src/data/file_menu_quit_ok.asm
@@ -1,11 +1,4 @@
 ; destination
-db $99, $C6
-; length
-db $01
-; tilemap
-db $A9, $6F
-
-; destination
 db $99, $D1
 ; length
 db $00
@@ -17,7 +10,7 @@ db $99, $E3
 ; length
 db $0E
 ; tilemap
-db $7E, $19, $14, $11, $14, $02, $0A, $7E, $7E, $7E, $0E, $0A, $00, $18, $3D
+db $7E, $00, $15, $01, $11, $18, $13, $7E, $7E, $7E, $0E, $0A, $7E, $7E, $7E
 
 ; end
 db $00

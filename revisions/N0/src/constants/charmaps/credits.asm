7ad8b98d0707975cc9e40ae9bfdf42819ac778bd
Tobias V. Langhoff <tobias@langhoff.no>
2023-09-27 23:19:11 +0200

    Fiks credits #33

--- revisions/N0/src/constants/charmaps/credits.asm
+++ revisions/N0/src/constants/charmaps/credits.asm
@@ -3,10 +3,4 @@ NEWCHARMAP CreditsCharmap
 
 SETCHARMAP AsciiCharmap
 
-; Credits-specific charmap patches for Norwegian
-charmap "[", $56 ; Æ
-charmap "]", $57 ; Ø
-charmap "-" $4E
-charmap ".", $4D
-
 POPC
